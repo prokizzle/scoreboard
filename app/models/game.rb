358fb5c07b05b06c3041f31c7922708e06097392
Nick Prokesch <nprokesch@gmail.com>
2017-09-16 17:27:59 +0000

    Override game json response

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,13 @@
 class Game < ApplicationRecord
     has_many :players
     has_many :points
+
+    def as_json(*)
+        {
+            id: id,
+            service_id: service_id,
+            start_time: start_time,
+            end_time: end_time,
+        }
+    end
 end
