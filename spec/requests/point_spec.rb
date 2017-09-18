0449ab404f16f98337c00d27b4569b007a0b3864
Nick Prokesch <nprokesch@gmail.com>
2017-09-18 17:39:44 +0000

    Fix points spec

--- spec/requests/point_spec.rb
+++ spec/requests/point_spec.rb
@@ -3,11 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Points API', type: :request do
     describe 'POST /points' do
         let (:points_attributes) { { point: { game_id: 1, player_id: 1 } } }
-        before { post '/points', params: points_attributes }
+        let (:game_attributes) { { players: [{first_name: 'Peter', last_name: 'Jackson'},{first_name: 'Peter', last_name: 'Pan', service: true}] } } 
+
+        before do 
+            post '/games', params: game_attributes
+            post '/points', params: points_attributes
+        end
 
         context 'when a point is scored' do
             it 'creates a point record' do
                 json = JSON.parse(response.body)
+                puts json.inspect
                 expect(json['point_id']).to eq(1)
                 expect(json['player_id']).to eq(1)
                 expect(json['scored_at']).to_not be(nil)
